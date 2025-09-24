eaf453acb4b341d45e5049649cd10a95c04cc8d1
gondsachin40 <gondsachin2006@gmail.com>
2025-09-24 13:47:47 +0530

    register

---
+++ src/app/register/register.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+interface ApiRes{
+  msg : string
+}
+@Component({
+  selector: 'app-register',
+  standalone : true,
+  imports: [FormsModule , HttpClientModule],
+  templateUrl: './register.html',
+  styleUrl: './register.css'
+})
+export class Register {
+ email: string = '';
+ password: string = '';
+ name : string = '';
+ constructor(private http: HttpClient) {}
+ onSubmit(){
+   const payload = {
+      name : this.name,
+      email : this.email,
+      password: this.password
+  };
+ console.log(payload);
+  this.http.post<ApiRes>('http://localhost:5000/api/auth/register', payload)
+      .subscribe({
+        next: (response ) => {
+          console.log('API Response:', response.msg);  
+        },
+        error: (error) => {
+          console.error('API Error:', error);       
+        }
+      });
+ }
+}
